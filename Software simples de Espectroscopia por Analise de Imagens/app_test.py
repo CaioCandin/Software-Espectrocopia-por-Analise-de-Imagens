ee56b4295d13ef75eab93c612d2b1e99f7da8f6f
Caio Augusto <candidobr123@gmail.com>
2025-05-02 21:18:24 -0300

    Alteração na interface

--- Software simples de Espectroscopia por Analise de Imagens/app_test.py
+++ Software simples de Espectroscopia por Analise de Imagens/app_test.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 import colour
 from colour.plotting import *
+from scipy.signal import find_peaks
 
 class EspectroscopiaApp:
     def __init__(self, root):
@@ -38,14 +39,21 @@ class EspectroscopiaApp:
             text='Espectro continuo',
             command=self.plotar_espectro_continuo,
         )
-        self.botao2.pack(pady= 10)
+        self.botao2.pack(pady= 1)
         
         self.botao3 = tk.Button(
             self.main_frame,
             text='CFMS',
-            command=plot_single_cmfs
+            command=self.plot_single_cmfs
         )
-        self.botao3.pack(pady= 10)
+        self.botao3.pack(pady= 1)
+        
+#        self.botao4 = tk.Button(
+#            self.main_frame,
+#            text='Absorbance',
+#            command=self.absorbance
+#        )
+#        self.botao4.pack(pady= 1)
         
         self.frame_graph = tk.Frame(self.main_frame, bg='white')
         self.frame_graph.pack(fill=tk.BOTH, expand=True)
@@ -53,6 +61,13 @@ class EspectroscopiaApp:
         self.label_imagem = tk.Label(self.main_frame)
         self.label_imagem.pack()
         
+    def plot_single_cmfs(self):
+        plot_single_cmfs(
+           "Stockman & Sharpe 2 Degree Cone Fundamentals",
+            y_label="Sensitivity",
+            bounding_box=(390, 870, 0, 1.1),
+        )
+    
     def carregar_imagem(self, value = None):
         
         filename = filedialog.askopenfilename(
@@ -61,31 +76,16 @@ class EspectroscopiaApp:
         if not filename:
             return
         
-        if value == 0:
-            self.img_arr = cv2.imread(filename)
-            self.img_arr = cv2.cvtColor(self.img_arr, cv2.COLOR_BGR2RGB)
-            
-            self.img = Image.fromarray(self.img_arr)
-            self.img = self.img.resize((875, 45))
-                
-            imagetk = ImageTk.PhotoImage(image=self.img)                
-            self.label_imagem.config(image=imagetk)
-            self.label_imagem.image = imagetk
-        elif value == 1:
-            self.img_arr = cv2.imread(filename)
-            self.img_arr = cv2.cvtColor(self.img_arr, cv2.COLOR_BGR2RGB)
-            
-            self.img = Image.fromarray(self.img_arr)
-            self.img = self.img.resize((875, 45))
-            self.img = self.img.rotate(360)    
-                
-            imagetk = ImageTk.PhotoImage(image=self.img)                
-            self.label_imagem.config(image=imagetk)
-            self.label_imagem.image = imagetk
-        else:
-            print("Valor não definido!")
+        self.img_arr = cv2.imread(filename)
+        self.img_arr = cv2.cvtColor(self.img_arr, cv2.COLOR_BGR2RGB)
         
-        
+        self.img = Image.fromarray(self.img_arr)
+        self.img = self.img.resize((775, 45))
+            
+        imagetk = ImageTk.PhotoImage(image=self.img)                
+        self.label_imagem.config(image=imagetk)
+        self.label_imagem.image = imagetk    
+            
     def plotar_espectro_rgb(self):
         
         self.carregar_imagem(value=0)
@@ -94,7 +94,7 @@ class EspectroscopiaApp:
             self.canvas.get_tk_widget().destroy()
         if self.toolbar:
             self.toolbar.destroy()
-                    
+        
         self.altura, self.largura, _ = self.img_arr.shape
         self.espectro_r = [np.mean(self.img_arr[:, x, 0])/255 for x in range(self.largura)]
         self.espectro_g = [np.mean(self.img_arr[:, x, 1])/255 for x in range(self.largura)]
@@ -117,47 +117,45 @@ class EspectroscopiaApp:
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
             
-        self.toolbar = NavigationToolbar2Tk(self.canvas, self.frame_graph)
-        self.toolbar.update()
+        #self.toolbar = NavigationToolbar2Tk(self.canvas, self.frame_graph)
+        #self.toolbar.update()
             
     def plotar_espectro_continuo(self):
-        self.carregar_imagem(value=1)
-        
+
         if self.canvas:
             self.canvas.get_tk_widget().destroy()
         if self.toolbar:
             self.toolbar.destroy()
         
         try:
-            altura, largura, _ = self.img_arr.shape
-        
-            R = np.array([np.mean(self.img_arr[:, x, 0])/255 for x in range(largura)])
-            G = np.array([np.mean(self.img_arr[:, x, 1])/255 for x in range(largura)])
-            B = np.array([np.mean(self.img_arr[:, x, 2])/255 for x in range(largura)])
+            altura, self.largura, _ = self.img_arr.shape
             
-            cmfs = colour.MSDS_CMFS["CIE 2015 10 Degree Standard Observer"]
+            cmfs = colour.MSDS_CMFS["Stockman & Sharpe 2 Degree Cone Fundamentals"]
             wavelengths = cmfs.wavelengths
             s_r = cmfs.values[:, 0]
             s_g = cmfs.values[:, 1]
             s_b = cmfs.values[:, 2]
             
-            comprimentos_onda_mapeados = np.linspace(380, 780, largura)
+            self.comprimentos_onda_mapeados = np.linspace(380, 780, self.largura)
+    
+            self.s_r_interp = np.interp(self.comprimentos_onda_mapeados, wavelengths, s_r)
+            self.s_g_interp = np.interp(self.comprimentos_onda_mapeados, wavelengths, s_g)
+            self.s_b_interp = np.interp(self.comprimentos_onda_mapeados, wavelengths, s_b)
             
-            sd = colour.SpectralDistribution()
+            espectro = (self.espectro_r * self.s_r_interp + self.espectro_g * self.s_g_interp + self.espectro_b * self.s_b_interp)
+
+            espectro /=  np.max(espectro)
             
-            s_r_interp = np.interp(comprimentos_onda_mapeados, wavelengths, s_r)
-            s_g_interp = np.interp(comprimentos_onda_mapeados, wavelengths, s_g)
-            s_b_interp = np.interp(comprimentos_onda_mapeados, wavelengths, s_b)
-            
-            espectro = (R * s_r_interp + G * s_g_interp + B * s_b_interp)
+            picos = find_peaks(espectro)
             
             self.fig = plt.Figure(figsize=(9, 4), dpi=100)
             ax = self.fig.add_subplot(111)
-            ax.plot(comprimentos_onda_mapeados, espectro, color='darkviolet')
+            ax.plot(self.comprimentos_onda_mapeados, espectro, color='darkviolet')
+            ax.scatter(self.comprimentos_onda_mapeados[picos[0]], espectro[picos[0]], color='black', alpha = 1)
             
             ax.set_xlabel("Comprimento de Onda (nm)")
             ax.set_ylabel("Intensidade Relativa")
-            ax.set_title("Espectro Reconstruído Dinâmico")
+            ax.set_title("Espectro estimado (ponderado pela percepção humana)")
             ax.set_xlim(380, 780)
             ax.grid(True, alpha=0.3)
             
@@ -165,8 +163,58 @@ class EspectroscopiaApp:
             self.canvas.draw()
             self.canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
             
+            #self.toolbar = NavigationToolbar2Tk(self.canvas, self.frame_graph)
+            #self.toolbar.update()
+
         except Exception as e:
             tk.messagebox.showerror("Erro", f"Falha ao gerar espectro:\n{str(e)}")
+        
+#    def absorbance(self):
+#        self.carregar_imagem(value=1)
+#    
+#        if self.canvas:
+#            self.canvas.get_tk_widget().destroy()
+#        if self.toolbar:
+#            self.toolbar.destroy()
+#        
+#        try:
+#            
+#            iluminantes = colour.SDS_ILLUMINANTS['D65']
+#            iluminantes_interp = np.interp(self.comprimentos_onda_mapeados, iluminantes.wavelengths, iluminantes.values)     
+#            
+#            R_c = np.maximum(np.sum(iluminantes_interp * self.s_r_interp), 1e-6)
+#            G_c = np.maximum(np.sum(iluminantes_interp * self.s_g_interp), 1e-6)
+#            B_c = np.maximum(np.sum(iluminantes_interp * self.s_b_interp), 1e-6)
+#            
+#            R = np.clip(self.espectro_r, 1e-6, 1.0)
+#            G = np.clip(self.espectro_g, 1e-6, 1.0)
+#            B = np.clip(self.espectro_b, 1e-6, 1.0)
+#            
+#            R_absorbance = -np.log10(R/R_c)
+#            G_absorbance = -np.log10(G/G_c)            
+#            B_absorbance = -np.log10(B/B_c)
+#            absorbance = (R_absorbance + G_absorbance + B_absorbance) / 3
+#
+#            self.fig = plt.Figure(figsize=(9, 4), dpi=100)
+#            ax = self.fig.add_subplot(111)
+#            
+#            ax.plot(self.comprimentos_onda_mapeados, absorbance, color='darkviolet')
+#            ax.set_xlabel("Comprimento de Onda (nm)")
+#            ax.set_ylabel("Intensidade Relativa")
+#            ax.set_title("Espectro Estimado")
+#            ax.set_xlim(380, 780)
+#            ax.grid(True, alpha=0.3)
+#            
+#            self.canvas = FigureCanvasTkAgg(self.fig, master=self.frame_graph)
+#            self.canvas.draw()
+#            self.canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+#            
+#            self.toolbar = NavigationToolbar2Tk(self.canvas, self.frame_graph)
+#            self.toolbar.update()
+#
+#        except Exception as e:
+#            tk.messagebox.showerror("Erro", f"Falha ao executar o comando:\n{str(e)}")
+        
         
 if __name__ == "__main__":
     root = tk.Tk()
